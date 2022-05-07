8bde6e7d5d93b19b40b9c6ee5e374d318df0258f
Brandon <wagnerbm@amazon.com>
2022-05-06 20:26:23 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bwagner5/ahem
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	delay := flag.Duration("delay", envOrDefault("delay", time.Second*60), "duration string to delay shutting down this application after a SIGTERM")
+	flag.Parse()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Printf("I've been interrupted with a %s!\n", sig)
+		fmt.Printf("Gracefully Shutting Down in %s\n", delay)
+		signalReceived := time.Now().UTC()
+		ticker := time.NewTicker(time.Second * 5)
+		for _ = range ticker.C {
+			durationUntilShutdown := *delay - time.Since(signalReceived)
+			if durationUntilShutdown <= 0 {
+				fmt.Println("👋 FINALLY SHUTTING DOWN! BYE!")
+				done <- true
+			}
+			fmt.Printf("Shutting down in %s\n", durationUntilShutdown)
+		}
+	}()
+
+	fmt.Println("awaiting SIGTERM signal")
+	<-done
+	fmt.Println("exiting")
+}
+
+func envOrDefault(key string, def time.Duration) time.Duration {
+	if val, ok := os.LookupEnv(key); ok {
+		dur, err := time.ParseDuration(val)
+		if err != nil {
+			return def
+		}
+		return dur
+	}
+	return def
+}
